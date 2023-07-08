9dd296b0409efe67605621ed5e97aa7a5857e441
harithriyadh <hareth.cn12012@gmail.com>
2023-07-08 18:55:00 +0300

    Done with the changes at src/app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
 const db = require('./db');
-const apiRoutes = require('./routes');
+const apiRoutes = require('./routes/authRoutes');
 
 const app = express();
+
 const port = process.env.NODE_LOCAL_PORT;
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', apiRoutes);
 
