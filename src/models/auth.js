b3218e88e72ed7e3c5e1447f2c30362041f422d5
harithriyadh <hareth.cn12012@gmail.com>
2023-07-08 10:30:28 +0300

    auth achemas are done

---
+++ src/models/auth.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const loginSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    required: true,
+  },
+});
+
+const User = mongoose.model( 'User', loginSchema );
+
+module.exports = { User };
