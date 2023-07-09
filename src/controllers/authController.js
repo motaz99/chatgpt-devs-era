3c0ab7935ce6dff3b9a1837701c109a5c5a10d65
motaz <motaz.dn.ali@gmail.com>
2023-07-09 21:59:14 +0300

    clean up the authController.js and fix the signup functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,40 +1,33 @@
 const bcrypt = require('bcrypt');
-const { User } = require('../models/auth');
+const NormalUser = require('../models/normalUser');
 require('dotenv').config();
-const { createTokens } = require('../jwt.js'); 
+const { createTokens } = require('../jwt');
 
 const login = async (req, res) => {
-  
   const { email, password } = req.body;
 
   try {
-
-    // checking if the user exist
-    const user = await User.findOne({ email });
+    const user = await NormalUser.findOne({ email });
     if (!user) {
-      return res.status(401).json({ error: 'Invalid email or password' });
+      res.status(401).json({ error: 'Invalid email or password' });
     }
 
-    // checking if the password valid
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Invalid  email or password' });
+      res.status(401).json({ error: 'Invalid  email or password' });
     }
 
-    // create JWT access token
     const accessToken = createTokens(user);
 
-    // save the JWT token to the cookies
     res.cookie('access-token', accessToken, {
       maxAge: 60 * 60 * 24 * 30 * 1000,
       httpOnly: true,
     });
 
-    // saving user information for middlewares 
+    // saving user information for middlewares
     req.user = user;
 
     res.json('logged in');
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -42,23 +35,22 @@ const login = async (req, res) => {
 };
 
 const signup = async (req, res) => {
-
-  const { name, email, password, role } = req.body;
+  const { firstname, lastname, email, password, role } = req.body;
 
   try {
-
     // checking if the user exist
-    const checkUser = await User.findOne({ email });
+    const checkUser = await NormalUser.findOne({ email });
     if (checkUser) {
-      return res.status(409).json({ error: 'Email is already registered' });
+      res.status(409).json({ error: 'Email is already registered' });
     }
 
     // checking if the password valid
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // saving user information to DB
-    const newUser = new User({
-      name,
+    const newUser = new NormalUser({
+      firstname,
+      lastname,
       email,
       password: hashedPassword,
       role,
@@ -67,15 +59,13 @@ const signup = async (req, res) => {
     const savedUser = await newUser.save();
 
     res.status(201).json(savedUser);
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-
-// logout 
+// logout
 const logout = async (req, res) => {
   res.clearCookie('access-token');
   res.send('Cookie deleted successfully');
@@ -85,50 +75,27 @@ const passwordReset = async (req, res) => {
   const { email, newPassword } = req.body;
 
   try {
-
-    // Find the user by email
-    const user = await User.findOne({ email });
-    console.log(user)
+    const user = await NormalUser.findOne({ email });
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      res.status(404).json({ error: 'User not found' });
     }
 
-    // incrypt the new password
     const hashedPassword = await bcrypt.hash(newPassword, 10);
 
-    // Set the new password
     user.password = hashedPassword;
 
-    // Save the updated user in the database
     await user.save();
 
-    // Password reset successful
     res.json({ message: 'Password reset successful' });
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-// endpoint to check if user is authorized or not
-const profile = (req, res) => {
-  console.log(req.userId);
-  res.json('profile');
-};
-
-// endpoint to check if user is authorized or not
-const dashboard = (req, res) => {
-  res.json('dashboard');
-};
-
-
-
 module.exports = {
   signup,
   login,
   logout,
-  profile,
-  dashboard,
   passwordReset,
 };
