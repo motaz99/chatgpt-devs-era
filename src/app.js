18ada9ed719363866fa85f85ee676236ae30cfc8
motaz <motaz.dn.ali@gmail.com>
2023-07-09 21:50:07 +0300

    add bodyparser middleware to make the request with body work

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const db = require('./db');
 const apiRoutes = require('./routes');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.NODE_LOCAL_PORT;
 
 app.use('/api', apiRoutes);
