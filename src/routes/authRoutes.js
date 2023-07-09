1694734c11fbadc49a08a5e3085ad23bd29c1a99
motaz <motaz.dn.ali@gmail.com>
2023-07-09 21:56:42 +0300

    clean up the authRoutes file

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const auth = require('../controllers/authController');
-const { validateToken } = require('../jwt.js');
+const { validateToken } = require('../jwt');
+
 const routes = express.Router();
 
-// checking if the user is authorized as an admin
-const isAdmin = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
-    next();
-  } else {
-    res.status(403).json({ error: 'You are not an admin.' });
-  }
-};
-
-routes.post('/signup', auth.signup); // Route to the signup function
-routes.post('/login', auth.login); // Route to the login function
-routes.post('/logout', auth.logout); // Route to the logout function
-routes.put('/passwordReset', validateToken, auth.passwordReset); // Route to the logout function
-
-// endpoints for testing the authentication and authorization
-routes.get('/profile', validateToken, auth.profile);
-routes.get('/dashboard', validateToken, isAdmin, auth.dashboard);
+routes.post('/signup', auth.signup);
+routes.post('/login', auth.login);
+routes.post('/logout', auth.logout);
+routes.put('/passwordReset', validateToken, auth.passwordReset);
 
 module.exports = routes;
