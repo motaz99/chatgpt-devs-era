db310836b3b941fde90ceac3165d68f26aeee529
harithriyadh <hareth.cn12012@gmail.com>
2023-07-12 21:18:16 +0300

    Done with the dishController endpoint, I added the crateDish endpoint

--- src/controllers/dishController.js
+++ src/controllers/dishController.js
@@ -3,11 +3,16 @@ const Dish = require('../models/dish');
 
 // Create a new dish
 exports.createDish = async (req, res) => {
+  const { name, description, price, rating } = req.body;
   try {
-    const { name, description, price, rating } = req.body;
+    const checkDish = await Dish.findOne({ name });
+    if (checkDish) {
+      throw new Error('Dish already exists...');
+    }
 
     // Create a new dish object
     const newDish = new Dish({
+      userId: req.session.userId,
       name,
       description,
       price,
@@ -18,10 +23,9 @@ exports.createDish = async (req, res) => {
     const savedDish = await newDish.save();
 
     // Send the saved dish as a response
-    res.status(201).json(savedDish);
+    res.status(201).json({ savedDish });
   } catch (error) {
-    // Handle server error
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json(error.message);
   }
 };
 
@@ -32,9 +36,24 @@ exports.getAllDishes = async (req, res) => {
     const dishes = await Dish.find();
 
     // Send the array of dishes as a response
-    res.json(dishes);
+    res.json({ dishes });
   } catch (error) {
     // Handle server error
     res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// get a dish
+exports.getDish = async (req, res) => {
+  const { id } = req.body;
+  try {
+    const checkDish = await Dish.findOne({ userId: id });
+    if (!checkDish) {
+      throw new Error('Dish does not exist...');
+    }
+
+    res.status(200).json({ dish: checkDish });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
