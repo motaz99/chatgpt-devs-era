c09627d92e65f218f90644231a7a89ed4397a17e
motaz <motaz.dn.ali@gmail.com>
2023-07-12 13:49:26 +0300

    delete not needed file jwt.js & use verifyUser middleware

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const auth = require('../controllers/authController');
-const { validateToken } = require('../jwt');
+const verifyUser = require('../middlewares/verifyUser');
 
 const routes = express.Router();
 
 routes.post('/signup', auth.signup);
 routes.post('/login', auth.login);
 routes.post('/logout', auth.logout);
-routes.put('/passwordReset', validateToken, auth.passwordReset);
+routes.put('/passwordReset', verifyUser, auth.passwordReset);
 
 module.exports = routes;
