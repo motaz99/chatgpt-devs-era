937c554515001061f8846899e969187998f448a8
motaz <motaz.dn.ali@gmail.com>
2023-07-16 15:20:43 +0300

    put the dish endpoints as sub endpoints for the chef

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 
 const routes = express.Router();
-const clientRoutes = require('./clients');
+const clientRoutes = require('./clientRoutes');
 const authRoutes = require('./authRoutes');
 const googleAuthRoutes = require('./googleAuthRoutes');
 const roleRoutes = require('./roleRoutes');
-const dishRoutes = require('./dishRoutes');
 const verifyUser = require('../middlewares/verifyUser');
-const chefRoutes = require('./chef');
+const chefRoutes = require('./chefRoutes');
 const orderRoutes = require('./order');
 
 routes.use('/googleAuth', googleAuthRoutes);
 routes.use('/auth', authRoutes);
 routes.use('/role', verifyUser, roleRoutes);
-routes.use('/dish', verifyUser, dishRoutes);
 routes.use('/chef', chefRoutes);
 routes.use('/clients', clientRoutes);
 routes.use('/orders', orderRoutes);
