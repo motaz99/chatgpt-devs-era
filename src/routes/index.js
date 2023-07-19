7620e7e3882a488378a502dd9ac6d39f5d1ad739
harithriyadh <hareth.cn12012@gmail.com>
2023-07-19 21:31:01 +0300

    Added the dishRoutes file to the index file routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ const roleRoutes = require('./roleRoutes');
 const verifyUser = require('../middlewares/verifyUser');
 const chefRoutes = require('./chefRoutes');
 const orderRoutes = require('./orderRoutes');
+const dishRoutes = require('./dishRoutes');
 
 routes.use('/googleAuth', googleAuthRoutes);
 routes.use('/auth', authRoutes);
@@ -15,5 +16,6 @@ routes.use('/role', verifyUser, roleRoutes);
 routes.use('/chef', verifyUser, chefRoutes);
 routes.use('/clients', verifyUser, clientRoutes);
 routes.use('/orders', verifyUser, orderRoutes);
+routes.use('/dish', verifyUser, dishRoutes); // No need to use "verifyClient", because this should be handled by the forntend.
 
 module.exports = routes;
