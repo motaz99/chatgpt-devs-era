65bf347e49f72402db51d1f7a7af2738c0e9f56b
motaz <motaz.dn.ali@gmail.com>
2023-07-19 13:04:19 +0300

    protect post order and get chef orders endpoints

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,10 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 const orderController = require('../controllers/orderController');
+const isUserCreatedChef = require('../middlewares/chefExistenceCheker');
+const isUserCreatedClient = require('../middlewares/clientExistenceCheker');
+const { verifyChef, verifyClient } = require('../middlewares/verfiyRole');
 
 router.use(express.json());
 
-router.post('/', orderController.postOrder);
-router.get('/chef-orders', orderController.getChefOrders);
+router.post('/', verifyClient, isUserCreatedClient, orderController.postOrder);
+router.get(
+  '/chef-orders',
+  verifyChef,
+  isUserCreatedChef,
+  orderController.getChefOrders
+);
 
 module.exports = router;
