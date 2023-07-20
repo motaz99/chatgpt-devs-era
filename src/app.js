714c6b469a9a8b8bd677408c545fc4a85df5fbfa
Özlem Keleş <ozlemkeles@Ozlem-MacBook-Pro.local>
2023-07-21 01:31:04 +0300

    Building the UI for Swagger

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const passportGoogleSetup = require('./passport-configuration/passport-google-setup');
+const swaggerOptions = require('./swagger/swagger');
 const db = require('./db');
 const apiRoutes = require('./routes');
 
@@ -15,6 +18,16 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const port = process.env.NODE_LOCAL_PORT;
+
+app.get('/api-docs.json', (req, res) => {
+  res.json(swaggerJsdoc(swaggerOptions));
+});
+
+app.use(
+  '/',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerOptions, { explorer: true })
+);
 
 app.use('/api', apiRoutes);
 
---
+++ src/swagger/swagger.js
@@ -0,0 +1,37 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Cookiz Project API with Swagger',
+      version: '1.0.0',
+      description:
+        'This is the documentation for all the functionalities in Cookiz Website',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/api',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['../routes/index.js'],
+};
+
+const spec = swaggerJsDoc(options);
+
+module.exports = spec;
