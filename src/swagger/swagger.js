d1f03f7fbc0c613ef6847e291989e0fcab49591b
Özlem Keleş <ozlemkeles@Ozlem-MacBook-Pro.local>
2023-07-23 22:06:26 +0300

    Added Chef Schema

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -42,6 +42,49 @@ const options = {
               example: 'client',
             },
           },
+          required: ['firstname', 'lastname', 'email', 'role'],
+        },
+        Chef: {
+          type: 'object',
+          properties: {
+            userId: {
+              type: 'string',
+              example: '64adddb8ffed160fd72dc69c',
+            },
+            restaurant: {
+              type: 'string',
+              example: 'Pizza House',
+            },
+            location: {
+              type: 'string',
+              example: '111 Cross Street',
+            },
+            openingHours: {
+              type: 'string',
+              example: '9:00 AM',
+            },
+            closingHours: {
+              type: 'string',
+              example: '6:00 PM',
+            },
+            contactNumber: {
+              type: 'string',
+              example: '+908887776655',
+            },
+            description: {
+              type: 'string',
+              example: 'Best pizza in the city',
+            },
+          },
+          required: [
+            'userId',
+            'restaurant',
+            'location',
+            'openingHours',
+            'closingHours',
+            'contactNumber',
+            'description',
+          ],
         },
       },
       securitySchemes: {
