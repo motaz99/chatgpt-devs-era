ed301afb7c23a26e8a3fbcf8ca54ba41dc2acf20
Harith Riyadh <55307099+harethriyadh@users.noreply.github.com>
2023-07-25 13:27:41 +0300

    Updated clientRoutes.js

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -28,7 +28,7 @@ router.get(
   clientController.getOrderHistory
 );
 router.get('/chefs', isUserCreatedClient, clientController.getChefs);
-router.post('/rate/:id', isUserCreatedClient, clientController.dishesRatings);
+router.post('/rating-dish/:id', isUserCreatedClient, clientController.dishesRatings);
 
 router.get('/chefs/:id', isUserCreatedClient, clientController.getChefById);
 
