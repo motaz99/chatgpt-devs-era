03ab61eee14634d738db1b8f42f0ecb72237925c
motaz <motaz.dn.ali@gmail.com>
2023-07-27 18:18:41 +0300

    Install ejs and start with simple template for the landing page

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ require('dotenv').config();
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const passportGoogleSetup = require('./passport-configuration/passport-google-setup');
 const db = require('./db');
 const apiRoutes = require('./routes');
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(passport.initialize());
 app.use(express.json());
@@ -17,6 +20,9 @@ app.use(bodyParser.json());
 const port = process.env.NODE_LOCAL_PORT;
 
 app.use('/api', apiRoutes);
+app.get('/home-page', (req, res) => {
+  res.render(`${__dirname}/views/index.ejs`);
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
