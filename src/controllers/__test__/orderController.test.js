88ba0ad2b497ad42b2bbdb4bb902b8e04abad07f
Özlem Keleş <ozlemkeles@Ozlem-MBP.home>
2023-08-05 01:24:27 +0300

    getChefOrders controller test done

--- src/controllers/__test__/orderController.test.js
+++ src/controllers/__test__/orderController.test.js
@@ -95,3 +95,89 @@ describe('postOrder', () => {
     });
   });
 });
+
+describe('getChefOrders', () => {
+  const chef = {
+    _id: 'chef12345',
+  };
+
+  const decodedToken = {
+    userId: 'user12345',
+  };
+
+  const chefOrders = [
+    {
+      _id: 'order1',
+      clientId: 'client1',
+      dishId: 'dish1',
+      quantity: 2,
+    },
+    {
+      _id: 'order2',
+      clientId: 'client2',
+      dishId: 'dish2',
+      quantity: 3,
+    },
+  ];
+
+  const req = {
+    cookies: {
+      jwt: 'token123',
+    },
+  };
+
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  beforeEach(() => {
+    Chef.findOne.mockClear();
+    Order.find.mockClear();
+    decodeJwtToken.mockReturnValue(decodedToken);
+  });
+
+  it('should retrieve chef orders', async () => {
+    Chef.findOne.mockResolvedValue(chef);
+    Order.find.mockResolvedValue(chefOrders);
+
+    await orderController.getChefOrders(req, res);
+
+    expect(Chef.findOne).toHaveBeenCalledTimes(1);
+    expect(Order.find).toHaveBeenCalledTimes(1);
+    expect(Order.find).toHaveBeenCalledWith({ chefId: chef._id });
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Here are your orders',
+      data: chefOrders,
+    });
+  });
+
+  it('should handle no orders', async () => {
+    Chef.findOne.mockResolvedValue(chef);
+    Order.find.mockResolvedValue([]);
+
+    await orderController.getChefOrders(req, res);
+
+    expect(Chef.findOne).toHaveBeenCalledTimes(1);
+    expect(Order.find).toHaveBeenCalledTimes(1);
+    expect(Order.find).toHaveBeenCalledWith({ chefId: chef._id });
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'You have no orders yet',
+    });
+  });
+
+  it('should handle error', async () => {
+    Chef.findOne.mockRejectedValue(new Error('Database error'));
+
+    await orderController.getChefOrders(req, res);
+
+    expect(Chef.findOne).toHaveBeenCalledTimes(1);
+    expect(Order.find).not.toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      error: 'Failed to retrieve chef orders',
+    });
+  });
+});
