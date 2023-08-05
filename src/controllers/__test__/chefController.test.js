4845a6e8f65f13344d6e2e8d2896d54f9612df3b
motaz <motaz.dn.ali@gmail.com>
2023-08-05 17:02:48 +0300

    Write first test case for create chef controller

---
+++ src/controllers/__test__/chefController.test.js
@@ -0,0 +1,53 @@
+const chefControllers = require('../chefController');
+const Chef = require('../../models/chef');
+const decodeJwtToken = require('../../helpers/decodeJwtToken');
+
+jest.mock('../../models/chef');
+jest.mock('../../helpers/decodeJwtToken');
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('createChef', () => {
+  it('should create a new chef and return a 201 status with a message', async () => {
+    const req = {
+      body: {
+        restaurant: 'Test Restaurant',
+        location: 'Test Location',
+        openingHours: '09:00 AM',
+        closingHours: '06:00 PM',
+        contactNumber: '1234567890',
+        description: 'Test Description',
+      },
+      cookies: {
+        jwt: 'mocked-jwt-token',
+      },
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      redirect: jest.fn(),
+    };
+
+    decodeJwtToken.mockReturnValue({ userId: 'mocked-user-id' });
+
+    Chef.findOne.mockResolvedValue(null);
+    Chef.create.mockResolvedValue({});
+
+    await chefControllers.createChef(req, res);
+
+    expect(Chef.findOne).toHaveBeenCalledTimes(1);
+    expect(Chef.create).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.redirect).toHaveBeenCalledWith('/api/chef/me');
+  });
+
+  it('should handle the case when the chef already exists', async () => {
+    // I need to add test case here
+  });
+
+  it('should handle errors and send a 500 status with error message', async () => {
+    // I need to add test case here
+  });
+});
